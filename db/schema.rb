de350c8d2c1d202bfdc2e08df378e122c2a608c5
Anthony Gladden <a.gladden360@outlook.com>
2022-10-21 21:10:32 -0700

    created reviews db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_20_161512) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_22_040440) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_20_161512) do
     t.string "name"
     t.integer "cost"
     t.string "contry_of_origin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "author"
+    t.string "content_body"
+    t.integer "rating"
+    t.integer "products_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
