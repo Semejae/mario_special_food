006940f6897d16ebbc913a56e17673f1b0644613
Anthony Gladden <a.gladden360@outlook.com>
2022-10-24 18:34:28 -0700

    added migration foreignkey

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_22_040440) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_25_012646) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,5 +30,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_22_040440) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-end
 
+  add_foreign_key "reviews", "products"
+end
