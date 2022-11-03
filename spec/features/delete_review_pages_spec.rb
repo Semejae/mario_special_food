268353202dc0da5db3e3560338c35f6f4128ccb6
Anthony Gladden <a.gladden360@outlook.com>
2022-11-02 19:11:40 -0700

    updated delete_review_pages_spec.rb

--- spec/features/delete_review_pages_spec.rb
+++ spec/features/delete_review_pages_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 describe "the delete a review process", type: :feature do 
   it "delete product review" do 
     test_product = Product.create(name: 'Mic', cost: '15', country_of_origin: 'USA')
-    test_review = Review.create(author: 'Anthony', content_body: 'blah', rating: 5, product_id: test_product.id)
+    test_review = Review.create(author: 'Anthony', content_body: 'Est qui est commodi praesentium unde omnis aut est cupiditate veniam pariatur iusto tempora et harum omnis non nihil quasi et quos a ab non.', rating: 5, product_id: test_product.id)
     visit product_review_path(test_product, test_review)
     click_on 'Delete review'
-    expect(page).not_to have_content 'blah'
+    expect(page).not_to have_content 'Est qui est commodi praesentium unde omnis aut est cupiditate veniam pariatur iusto tempora et harum omnis non nihil quasi et quos a ab non.'
   end 
 end
--- spec/features/update_review_pages_spec.rb
+++ spec/features/update_review_pages_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 describe "update review process", type: :feature do 
   it "update product review" do
     test_product = Product.create(name: 'Mic', cost: '15', country_of_origin: 'USA')
-    test_review = Review.create(author: 'Anthony', content_body: 'blah', rating: 5, product_id: test_product.id)
+    test_review = Review.create(author: 'Anthony', content_body: 'Est qui est commodi praesentium unde omnis aut est cupiditate veniam pariatur iusto tempora et harum omnis non nihil quasi et quos a ab non.', rating: 5, product_id: test_product.id)
     visit product_review_path(test_product, test_review)
     click_on 'Edit review'
     fill_in 'Author', with: 'Kacie'
-    fill_in 'Content body', with: 'blah'
+    fill_in 'Content body', with: 'Est qui est commodi praesentium unde omnis aut est cupiditate veniam pariatur iusto tempora et harum omnis non nihil quasi et quos a ab non.'
     fill_in 'Rating', with: 5
     click_on 'Update Review'
     expect(page).to have_content 'Kacie'
