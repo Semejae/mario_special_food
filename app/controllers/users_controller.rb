80667ed24fa4cf39b3ed5766ff48bd255e1a9956
Anthony Gladden <a.gladden360@outlook.com>
2022-11-13 01:39:00 -0800

    updated controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,3 +8,28 @@ class UserController < ApplicationController
   def show 
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    respond_to do |format|
+      if current_user.update(user_params)
+        format.html {redirect_to current_user, notice: "successfully updated!"}
+      else
+        format.html { render :edit}
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :bio,
+      :first_name,
+      :last_name
+    )
+  end
+end
