9e6ec4fd62403aa7a7395c45c917961fb867bb55
Anthony Gladden <a.gladden360@outlook.com>
2022-11-13 02:18:07 -0800

    updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'products#index'
+  mount RailsAdmin: :Engine => '/admin', as: 'rails_admin'
+  devise_for :users 
+  resources :users, only: [:index, :show, :edit, :update]
+  root to: 'products#homepage'
   resources :products do
     resources :reviews
   end
