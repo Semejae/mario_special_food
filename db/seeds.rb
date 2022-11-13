75bc7970a2d4550222dec905ea766aa3750d043d
Anthony Gladden <a.gladden360@outlook.com>
2022-11-13 00:59:12 -0800

    updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,3 +21,18 @@ Review.destroy_all
                     product_id: product.id)
   end
 end
+
+1.times do |index|
+  user.create!({ email: Faker::Internet.email(name:
+  Faker::Name.name ), password: "passwsord", first_name:Faker::Name.name, last_name: Faker::Name.last_name, bio: Faker::Lorem.paragraph_by_chars(number: 120) })
+end
+
+1.times do |index|
+  user.create!({ email: Faker::Internet.email(name:
+  Faker::Name.name ), password: "passwsord", first_name:Faker::Name.name, last_name: Faker::Name.last_name, bio: Faker::Lorem.paragraph_by_chars(number: 120), admin: true })
+end
+
+p "Created #{Product.count} products"
+p "Created #{Review.count} reviews"
+p "Created #{User.count} users"
+
